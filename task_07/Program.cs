88ffd9695bfdff76a7cccb39d0fc5292e88261fd
Andrey Linkov <linkov.developer@inbox.ru>
2024-02-25 23:32:35 +0300

    add task_07

---
+++ task_07/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+
+namespace task_07
+{
+    class Program
+    {
+        public static void FillArray(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+        }
+
+        public static void ReverseArray(int[] array, int[] reverseArray)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                reverseArray[i] = array[array.Length - 1 - i];
+            }
+        }
+
+        static void Main()
+        {
+            Console.Clear();
+
+            Random random = new Random();
+            int[] array = new int[random.Next(5, 10)];
+            FillArray(array);
+            Console.WriteLine(string.Join(", ", array));
+            // Более простой способ разворота массива
+
+            // Array.Reverse(array);
+            // Console.WriteLine(string.Join(", ", array));
+
+            int[] reverseArray = new int[array.Length];
+            ReverseArray(array, reverseArray);
+            Console.WriteLine(string.Join(", ", reverseArray));
+        }
+    }
+}
